abd04fecb83b93643877aebdfc11e5050b9ea748
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-11-08 15:11:03 +0700

    Succesfully Connected to MongoDB

--- index.js
+++ index.js
@@ -3,15 +3,45 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middlewar 
 app.use(cors());
 app.use(express.json());
 
+// Connect with MongoDB.
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@s-cluster0.pvehgyn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Get Data
 app.get('/', (req, res) => {
     res.send('Welcom to Indian Royal');
 });
 
+// Listen Data
 app.listen(port, () => {
     console.log(`Indian Royal Resturent's server is running on port ${port}`);
 });
