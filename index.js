f14d78e4539cc73af17a16f6dcef0079c2271f73
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-05-15 21:50:45 +0700

    Updated Code

--- index.js
+++ index.js
@@ -28,19 +28,12 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         // Databse
-        const menuData = client.db("indianRoyalDB").collection('menu');
-
-        app.get('/menu',async (req, res)=> {
-            const menu = menuData.find().toArray();
-            res.send(menu);
-        })
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+        return client.db("indianRoyalDB").collection('menu');
+    }
+    finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
